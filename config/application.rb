afda07de3c3d5140eee0e363203b7798ef6e4439
Dorian Marié <dorian@thoughtbot.com>
2022-06-29 00:49:22 +0200

    french translations

--- config/application.rb
+++ config/application.rb
@@ -4,12 +4,12 @@ require "rails/all"
 
 Bundler.require(*Rails.groups)
 
-module Money
+module Pipelines
   class Application < Rails::Application
     config.load_defaults 7.0
     config.generators.test_framework nil
     config.i18n.default_locale = :en
     config.i18n.available_locales = %i[en fr]
-    config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
+    config.action_mailer.default_url_options = {host: ENV.fetch("HOST")}
   end
 end
