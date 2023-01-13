755f79c28e283abba74fa1ff03a9d02eb1d1674a
Dorian Marié <dorian@dorianmarie.fr>
2023-01-13 13:11:40 +0100

    remove redis session store

--- config/application.rb
+++ config/application.rb
@@ -10,23 +10,13 @@ module Pipelines
     config.generators.test_framework nil
     config.i18n.default_locale = :en
     config.i18n.available_locales = %i[en fr]
-    config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
+    config.action_mailer.default_url_options = {host: ENV.fetch("HOST")}
     config.assets.css_compressor = nil
     config.active_job.queue_adapter = :sidekiq
 
     config.after_initialize do |app|
       app.routes.default_url_options =
         app.config.action_mailer.default_url_options
-    end
-
-    if ENV["SESSION_REDIS_URL"]
-      config.session_store :redis_session_store,
-                           key: "_pipelines_session",
-                           redis: {
-                             url: ENV.fetch("SESSION_REDIS_URL"),
-                             key_prefix: "pipelines:session:",
-                             expire_after: 3.months
-                           }
     end
   end
 end
