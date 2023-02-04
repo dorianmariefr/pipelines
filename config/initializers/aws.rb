76a2894ad88b57ce9850fb1b7bb742fc25c1d366
Dorian Marié <dorian@dorianmarie.fr>
2023-02-04 08:20:43 +0100

    only setup aws on production

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,9 +1,11 @@
-Aws::Rails.add_action_mailer_delivery_method(
-  :ses,
-  credentials:
-    Aws::Credentials.new(
-      Rails.application.credentials.aws[:access_key_id],
-      Rails.application.credentials.aws[:secret_access_key]
-    ),
-  region: Rails.application.credentials.aws[:region]
-)
+if Rails.env.production?
+  Aws::Rails.add_action_mailer_delivery_method(
+    :ses,
+    credentials:
+      Aws::Credentials.new(
+        Rails.application.credentials.aws.access_key_id,
+        Rails.application.credentials.aws.secret_access_key
+      ),
+    region: Rails.application.credentials.aws.region
+  )
+end
