2788f5c8c557f74a72fd69b0fee8f5cd9ce711d7
Dorian Marié <dorian@dorianmarie.fr>
2023-02-04 07:42:08 +0100

    remove migration

--- db/migrate/20230204062604_delete_duplicate_items.rb
+++
@@ -1,8 +0,0 @@
-class DeleteDuplicateItems < ActiveRecord::Migration[7.0]
-  def change
-    Item
-      .preload(:pipeline)
-      .group_by { |item| [item.external_id, item.pipeline.id] }
-      .each { |_, items| items[1..].each(&:destroy) }
-  end
-end
