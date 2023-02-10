ac1f7739061cfbee257976edf64fc8353857f4fb
Dorian Marié <dorian@dorianmarie.fr>
2023-02-10 10:04:20 +0100

    ignore deserialization errors

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,8 @@ class ApplicationMailer < ActionMailer::Base
   default from: "Dorian Marié <dorian@pipelines.plumbing>"
   layout "mailer"
 
+  rescue_from ActiveJob::DeserializationError
+
   def to(email)
     email_address_with_name(email.email, email.user.name)
   end
